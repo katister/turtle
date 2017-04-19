c677fd47d0331f7e5f6c92b8725cc3e3ad68f600
katister <katisterw@gmail.com>
2017-04-19 00:36:57 -0400

    Basic ocean scene start with turtle egg

---
+++ turtle/app.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+var sphere = document.querySelector('a-sphere');
+sphere.getAttribute('position');
+sphere.addEventListener('click', function () {
+  sphere.setAttribute('color', 'red');
+});
+
+
+background: #c2e59c;
+background: -webkit-linear-gradient(to right, #64b3f4, #c2e59c);
+background: linear-gradient(to right, #64b3f4, #c2e59c); 
