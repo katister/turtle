5da8ed9f01d43de515aa3ed2f493e5fa45180530
katister <katisterw@gmail.com>
2017-04-19 01:04:52 -0400

    Added instruction text

--- turtle/app.js
+++ turtle/app.js
@@ -1,12 +1,7 @@
-console.log("hello");
-
-var sphere = document.querySelector('a-sphere');
-sphere.getAttribute('position');
-sphere.addEventListener('click', function () {
-  sphere.setAttribute('color', 'red');
-});
-
-
-background: #c2e59c;
-background: -webkit-linear-gradient(to right, #64b3f4, #c2e59c);
-background: linear-gradient(to right, #64b3f4, #c2e59c); 
+// console.log("hello");
+//
+// var sphere = document.querySelector('a-sphere');
+// sphere.getAttribute('position');
+// sphere.addEventListener('click', function () {
+//   sphere.setAttribute('color', 'red');
+// });
