8118d6ffeb991e56eff76ef4b8c65e61a2183b34
katister <katisterw@gmail.com>
2017-04-23 21:12:11 -0400

    improved lighting and defined variables in js

--- turtle/app.js
+++ turtle/app.js
@@ -5,3 +5,26 @@
 // sphere.addEventListener('click', function () {
 //   sphere.setAttribute('color', 'red');
 // });
+
+const scene = document.querySelector('a-scene')
+const sky = document.querySelector('a-sky')
+const objectContainer = document.querySelector('#object-container')
+const sphere = document.querySelector('a-sphere')
+const turtle = document.querySelector(tbd)
+
+function getRandomNumber(x, y) {
+  return Math.floor(Math.random() * x + y)
+}
+
+
+const totalSteps = getRandomNumber(17, 10)
+const totalRotations = getRandomNumber(17, 10)
+
+function getRandomColor() {
+  const letters = '0123456789abcdef'
+  let randomColor = ''
+  for (let i = 0; i < 6; i++) {
+    randomColor += letters[Math.floor(Math.random() * 16)]
+  }
+  return randomColor
+}
